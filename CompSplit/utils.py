89315d41e11b0579176891b9f285ef6444693c41
saraitne <saraitne11@naver.com>
2020-02-03 11:02:08 +0900

    word2idx update

--- CompSplit/utils.py
+++ CompSplit/utils.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 ALPHABET = {c: i for i, c in enumerate('Pabcdefghijklmnopqrstuvwxyz')}
+PAD = 'P'
 
 
-def word2idx(word, dtype=np.uint8):
+def word2idx(word, max_len, dtype=np.uint8):
+    word = word + PAD * (max_len-len(word))
     return np.array(list(map(lambda c: ALPHABET[c], word)), dtype=dtype)
