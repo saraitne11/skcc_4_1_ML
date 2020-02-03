ae069050e546269480b38c85c2fe2dd726cbda46
HAK <bbq3438@naver.com>
2020-02-03 11:40:19 +0900

    얼굴인식전처리

--- FaceRecog/faceRecog.py
+++ FaceRecog/faceRecog.py
@@ -2,35 +2,15 @@ from PIL import Image
 import numpy as np
 import os
 import csv
+import random
 
-
-def readData():
-    dataList = []
-    imagePath = "../_Data/1. 얼굴사진분류 데이터/image_v2/face_images_128x128/"
-    trainPath = "../_Data/1. 얼굴사진분류 데이터/ml_8_faceclassifier_train.csv"
-
-    f = open(trainPath, 'r', encoding="utf-8")
-    reader = list(csv.reader(f))
-    reader.pop(0)
-    f.close()
-    for line in reader:
-        tempList = []
-        tempList.append(line[0])
-        tempList.append(line[1])
-        img = Image.open(imagePath + tempList[0], "r")
-        tempList.append(np.array(img))
-        dataList.append(tempList)
-        print(tempList)
-
-    # 파일이름, 레이블, 이미지데이터
-    return dataList
 
 def main():
-    data = readData()
+    pass
+
 
 if __name__ == "__main__":
     main()
-
 
 # TODO
 # ReadData 클래스로 만들기
@@ -43,7 +23,7 @@ if __name__ == "__main__":
 #       self.num_data = 데이터 개수
 #
 # def random_batch(batch_size):
-#       모델 학습용
+#       모델 학습용, 에포크
 #       x = 랜덤한 batch_size개의 이미지 데이터    [batch_size, 128, 128, 3]
 #       y = 랜덤한 batch_size개의 레이블          [batch_size]
 #           이미지 데이터와 레이블이 매칭 되어야함
@@ -51,4 +31,38 @@ if __name__ == "__main__":
 #
 # def sequential_batch(batch_size):
 #       모델 테스트용
+<<<<<<< Updated upstream
 #       return x, file_name   csv 만들어야 되니까
+=======
+#       return x
+
+
+class dataSet:
+
+    def __init__(self, imagePath, csvPath):
+        # imagePath = "../_Data/1. 얼굴사진분류 데이터/image_v2/face_images_128x128/"
+        # csvPath = "../_Data/1. 얼굴사진분류 데이터/ml_8_faceclassifier_train.csv"
+
+        self.imageData = []
+        self.imageLable = []
+        self.fileName = []
+        self.numData = None
+
+        f = open(csvPath, 'r', encoding="utf-8")
+        reader = list(csv.reader(f))
+        reader.pop(0)
+        f.close()
+        self.numData = len(reader)
+
+        for line in reader:
+            self.fileName.append(line[0])
+            self.imageLable.append(line[1])
+            img = Image.open(imagePath + line[0], "r")
+            self.imageData.append(np.array(img))
+
+    def randomBatch(self):
+        pass
+
+    def sequential_batch(self):
+        pass
+>>>>>>> Stashed changes
